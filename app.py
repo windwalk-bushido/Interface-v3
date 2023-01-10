ff68b8101b474da4ab5ec2193e54a93b140117b8
Milijan Mosic <milijan-mosic@protonmail.com>
2023-01-10 23:53:58 +0100

    Removed unwanted code

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-
 
 
 class Website():
@@ -21,28 +16,43 @@ class Website():
     self.url = url
 
 
+
+
 def ReadDataFromFile(file, array):
-  file = open(file, "rt")
+  file = open(file, 'rt')
   lines = file.readlines()
+
   for i in range(0, len(lines), 3):
-    website = Website(lines[i].strip("\n"), lines[i + 1].strip("\n"))
+    website = Website(lines[i].strip('\n'), lines[i + 1].strip('\n'))
     array.append(website)
+
   file.close()
+
+
+
 
 def ReadData():
   for i in range(0, 6):
     if(i == 0):
-      ReadDataFromFile("data/frontend.txt", frontend)
+      ReadDataFromFile('data/frontend.txt', frontend)
     if(i == 1):
-      ReadDataFromFile("data/documentation.txt", documentation)
+      ReadDataFromFile('data/documentation.txt', documentation)
     if(i == 2):
-      ReadDataFromFile("data/backend.txt", backend)
+      ReadDataFromFile('data/backend.txt', backend)
     if(i == 3):
-      ReadDataFromFile("data/job.txt", job)
+      ReadDataFromFile('data/job.txt', job)
     if(i == 4):
-      ReadDataFromFile("data/useful.txt", useful)
+      ReadDataFromFile('data/useful.txt', useful)
     if(i == 5):
-      ReadDataFromFile("data/social.txt", social)
+      ReadDataFromFile('data/social.txt', social)
+
+
+
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
 
 
 frontend = []
@@ -55,29 +65,39 @@ social = []
 ReadData()
 
 
-@app.get("/", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
+
+
+@app.get('/', response_class=HTMLResponse, status_code=status.HTTP_200_OK)
 async def index(request: Request):
   return templates.TemplateResponse(
-    "index.html",
-      {"request": request,
-      "frontend": frontend,
-      "documentation": documentation,
-      "backend": backend,
-      "job": job,
-      "useful": useful,
-      "social": social}
+    'index.html', {
+      'request': request,
+      'frontend': frontend,
+      'documentation': documentation,
+      'backend': backend,
+      'job': job,
+      'useful': useful,
+      'social': social
+      }
   )
+
+
+
 
 @app.get('/{file}', response_class=FileResponse, status_code=status.HTTP_200_OK)
 async def serve_static(file: str):
-  if file == "robots.txt" or file == "humans.txt":
-    path = "static/global/" + file
+  if file == 'robots.txt' or file == 'humans.txt':
+    path = 'static/global/' + file
     return FileResponse(path=path)
   else:
-    return RedirectResponse("https://windwalks-interface.herokuapp.com/")
+    return RedirectResponse('/') # will this work?
 
-""" Not working... every bad request will redirect user to index page.
-@app.get("/404", response_class=HTMLResponse, status_code=404)
+
+
+
+'''
+# Not working... every bad request will redirect user to index page.
+@app.get('/404', response_class=HTMLResponse, status_code=404)
 async def raise_404(request: Request):
-  return templates.TemplateResponse('404.html', {"request": request})
-"""
+  return templates.TemplateResponse('404.html', {'request': request})
+'''
